109f99d0913911537499d8fffc5ed9eb316cdf41
Blueturtle <104430690+Blueturtle08@users.noreply.github.com>
2024-04-03 08:57:32 +0900

    secondchange

--- EDmain.py
+++ EDmain.py
@@ -1,4 +1,5 @@
-num = int(input("정수 입력:"))
+print("369게임에 오신 것을 환영합니다\n숫자에 3, 6, 9가 포함되어있으면 \'짝\'이 출력됨니다")
+num = int(input("정수 입력: "))
 cnt = 0
 
 for i in range(1, num+1, 1):
@@ -11,7 +12,6 @@ for i in range(1, num+1, 1):
             print("짝", end=' ')
             cnt += 1
             break
-        
         last = int(last / 10)
 
-print(cnt)
+print("\n짝이 나온 횟수: %d" % cnt)
